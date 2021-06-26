098f0a2ecdce96e060534fc502ecc8289fed84f8
Varadjanin <mbodrozic@gmail.com>
2021-06-26 17:06:03 +0200

    adding more exercises

--- ES6/airport.js
+++ ES6/airport.js
@@ -4,22 +4,20 @@
 		if (!name || !surname) {
 			throw new Error('Fields name and surname are required');
 		}
-		this.name = name
-		this.surname = surname
+		this.name = name;
+		this.surname = surname;
 		this.getData = function() {
-			return this.name + ' ' + this.surname
+			return `${this.name} ${this.surname}`;
 		}
 	}
-	function Seat(number, category) {
-		category = category || "e";
-		number = number || Math.floor( 91 * Math.random() + 10 )
+	function Seat(number = Math.floor( 91 * Math.random() + 10 ), category = "e") {
 		if ( !['e','b'].includes(category) ) {
 			throw new Error('Invalid category input');
 		}
-		this.number = number
+		this.number = number;
 		this.category = category;
 		this.getData = function() {
-			return this.number + ', ' + this.category.toUpperCase();
+			return `${this.number} ${this.category.toUpperCase()}`;
 		}
 	}
 	function Passenger(person, seat) {
@@ -29,77 +27,80 @@
 		if (!seat ||  !(seat instanceof Seat)) {
 			throw new Error('Invalid seat input');
 		}
-		this.person = person
-		this.seat = seat
+		this.person = person;
+		this.seat = seat;
 		this.getData = function () {
-			return this.seat.getData() + ', ' + this.person.getData()
+			return `${this.seat.getData()} ${this.person.getData()}`;
 		}
 	}
 	function Flight(relation, date) {
 		if (!relation || !date) {
 			throw new Error('Fields relation and date are required');
 		}
-		this.relation = relation
-		this.date = new Date(date)
-		this.listOfPassengers = []
+		this.relation = relation;
+		this.date = new Date(date);
+		this.listOfPassengers = [];
 		this.addPassenger = function(passenger) {
 			if (!passenger || !(passenger instanceof Passenger)) {
-				throw new Error('Invalid passenger data')
+				throw new Error('Invalid passenger data');
 			}
 			this.listOfPassengers.push(passenger);
 		}
 		this.getData = function() {
-			var year = this.date.getFullYear();
-			var month = this.date.getMonth() + 1;
-			var day = this.date.getDate();
-			var result = '\t' + day + '.' + month + '.' + year + ', ' + this.relation + '\n';
+			let year = this.date.getFullYear();
+			let month = this.date.getMonth() + 1;
+			let day = this.date.getDate();
+			let result = `  ${day}.${month}.${year}, ${this.relation}
+            `;
 			this.listOfPassengers.forEach(function(passenger) {
-				result += '\t\t' + passenger.getData() + '\n'
+				result += `     ${passenger.getData()}
+                `;
 			})
 			return result;
 		}
 	}
 	function Airport() {
-		this.name = "Nikola Tesla"
-		this.listOfFlights = []
+		this.name = "Nikola Tesla";
+		this.listOfFlights = [];
 		this.addFlight = function(flight) {
 			if (!flight || !(flight instanceof Flight)) {
-				throw new Error('Invalid flight data')
+				throw new Error('Invalid flight data');
 			}
 			this.listOfFlights.push(flight);
 		}
 		this.getPassengersNumber = function() {
-			var count = 0;
+			let count = 0;
 			this.listOfFlights.forEach(function(flight) {
-				count += flight.listOfPassengers.length
+				count += flight.listOfPassengers.length;
 			})
 			return count;
 		}
 		this.getData = function() {
-			var result = 'Airport: ' + this.name + ', total passengers: ' + this.getPassengersNumber() + '\n';
+			let result = `Airport: ${this.name}, total passengers: ${this.getPassengersNumber()}
+            `;
 			this.listOfFlights.forEach(function(flight) {
 				result += flight.getData();
 			})
-			return result
+			return result;
 		}
 	}
 	function createFlight(relation, date) {
-		return new Flight(relation, date)
+		return new Flight(relation, date);
 	}
 	function createPassenger(firstName, lastName, seatNumber, category) {
-		var person = new Person(firstName, lastName)
-		var seat = new Seat(seatNumber, category);
+		let person = new Person(firstName, lastName);
+		let seat = new Seat(seatNumber, category);
 		return new Passenger(person, seat);
 	}
 	//testing
 	try {
-		var nikolaTesla = new Airport()
-		var belgradeParis = createFlight("Belgrade - Paris", "Oct 25 2017")
-		var barcelonaBelgrade = createFlight("Barcelona - Belgrade", "Nov 11 2017")
-		var passenger1 = createPassenger("Dario", "Stamenkovic", 67, "b")
-		var passenger2 = createPassenger("Cersei", "Lannister", 62, "b")
-		var passenger3 = createPassenger("Daenerys", "Targaryen", 14)
-		var passenger4 = createPassenger("Nikola", "Colovic")
+		let nikolaTesla = new Airport()
+		let belgradeParis = createFlight("Belgrade - Paris", "Oct 25 2017")
+		let barcelonaBelgrade = createFlight("Barcelona - Belgrade", "Nov 11 2017")
+		let passenger1 = createPassenger("Dario", "Stamenkovic", 67, "b")
+		let passenger2 = createPassenger("Cersei", "Lannister", 62, "b")
+		let passenger3 = createPassenger("Daenerys", "Targaryen", 14)
+		let passenger4 = createPassenger("Nikola", "Colovic")
 		belgradeParis.addPassenger(passenger1)
 		belgradeParis.addPassenger(passenger2)
 		barcelonaBelgrade.addPassenger(passenger3)
@@ -108,6 +109,6 @@
 		nikolaTesla.addFlight(barcelonaBelgrade)
 		console.log(nikolaTesla.getData());
 	} catch (error) {
-		console.log('Error message:' + error.message);
+		console.log(`Error message: ${error.message}`);
 	}
 })()
