ceb3e33c7711457f81100de96584edc8f0273eb2
Varadjanin <mbodrozic@gmail.com>
2021-07-01 23:01:14 +0200

    changing constructors to ES6

--- ES6/airport.js
+++ ES6/airport.js
@@ -1,89 +1,106 @@
 "use strict";
 (function () {
-	function Person(name, surname) {
-		if (!name || !surname) {
-			throw new Error('Fields name and surname are required');
-		}
-		this.name = name;
-		this.surname = surname;
-		this.getData = function() {
+	class Person {
+		constructor(name, surname) {
+			// if (!name || !surname) {
+			// 	throw new Error('Fields name and surname are required');
+			// };
+			this.name = name;
+			this.surname = surname;
+		};
+		getData() {
 			return `${this.name} ${this.surname}`;
-		}
-	}
-	function Seat(number = Math.floor( 91 * Math.random() + 10 ), category = "e") {
-		if ( !['e','b'].includes(category) ) {
-			throw new Error('Invalid category input');
-		}
-		this.number = number;
-		this.category = category;
-		this.getData = function() {
+		};
+	};
+
+	class Seat {
+		constructor(number = Math.floor( 91 * Math.random() + 10 ), category = "e") {
+			// if ( !['e','b'].includes(category) ) {
+			// 	throw new Error('Invalid category input');
+			// }
+			this.number = number;
+			this.category = category;
+		};
+		getData() {
 			return `${this.number} ${this.category.toUpperCase()}`;
-		}
-	}
-	function Passenger(person, seat) {
-		if (!person ||  !(person instanceof Person)) {
-			throw new Error('Invalid person input');
-		}
-		if (!seat ||  !(seat instanceof Seat)) {
-			throw new Error('Invalid seat input');
-		}
-		this.person = person;
-		this.seat = seat;
-		this.getData = function () {
+		};
+	};
+
+	class Passenger {
+		constructor(person, seat) {
+			// if (!person ||  !(person instanceof Person)) {
+			// 	throw new Error('Invalid person input');
+			// }
+			// if (!seat ||  !(seat instanceof Seat)) {
+			// 	throw new Error('Invalid seat input');
+			// }
+			this.person = person;
+			this.seat = seat;
+		};
+		getData() {
 			return `${this.seat.getData()} ${this.person.getData()}`;
-		}
-	}
-	function Flight(relation, date) {
-		if (!relation || !date) {
-			throw new Error('Fields relation and date are required');
-		}
-		this.relation = relation;
-		this.date = new Date(date);
-		this.listOfPassengers = [];
-		this.addPassenger = function(passenger) {
-			if (!passenger || !(passenger instanceof Passenger)) {
-				throw new Error('Invalid passenger data');
-			}
+		};
+	};
+	
+	class Flight {
+		constructor(relation, date) {
+			// if (!relation || !date) {
+			// 	throw new Error('Fields relation and date are required');
+			// }
+			this.relation = relation;
+			this.date = new Date(date);
+			this.listOfPassengers = [];
+		};
+		addPassenger(passenger) {
+			// if (!passenger || !(passenger instanceof Passenger)) {
+			// 	throw new Error('Invalid passenger data');
+			// }
 			this.listOfPassengers.push(passenger);
-		}
-		this.getData = function() {
+		};
+		getData() {
 			let year = this.date.getFullYear();
 			let month = this.date.getMonth() + 1;
 			let day = this.date.getDate();
 			let result = `  ${day}.${month}.${year}, ${this.relation}
-            `;
+			`;
 			this.listOfPassengers.forEach(function(passenger) {
 				result += `     ${passenger.getData()}
-                `;
-			})
+				`;
+			});
 			return result;
-		}
-	}
-	function Airport() {
-		this.name = "Nikola Tesla";
-		this.listOfFlights = [];
-		this.addFlight = function(flight) {
-			if (!flight || !(flight instanceof Flight)) {
-				throw new Error('Invalid flight data');
-			}
+		};
+	};
+	
+	class Airport {
+		constructor() {
+			this.name = "Nikola Tesla";
+			this.listOfFlights = [];
+		};
+		addFlight(flight) {
+			// if (!flight || !(flight instanceof Flight)) {
+			// 	throw new Error('Invalid flight data');
+			// }
 			this.listOfFlights.push(flight);
-		}
-		this.getPassengersNumber = function() {
+		};
+		getPassengersNumber() {
 			let count = 0;
 			this.listOfFlights.forEach(function(flight) {
 				count += flight.listOfPassengers.length;
-			})
+			});
 			return count;
 		}
-		this.getData = function() {
+		getData() {
 			let result = `Airport: ${this.name}, total passengers: ${this.getPassengersNumber()}
             `;
 			this.listOfFlights.forEach(function(flight) {
 				result += flight.getData();
-			})
+			});
 			return result;
-		}
-	}
+		};
+	};
+	
+
+
 	function createFlight(relation, date) {
 		return new Flight(relation, date);
 	}
@@ -92,6 +109,7 @@
 		let seat = new Seat(seatNumber, category);
 		return new Passenger(person, seat);
 	}
+	
 	//testing
 	try {
 		let nikolaTesla = new Airport()
