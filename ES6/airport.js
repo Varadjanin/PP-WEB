a3e13a95899c68fdda5768153d6594f16efec2f6
Varadjanin <mbodrozic@gmail.com>
2021-07-03 13:09:43 +0200

    practicing ES6 code writing

--- ES6/airport.js
+++ ES6/airport.js
@@ -1,132 +1,131 @@
 "use strict";
 (function () {
-	class Person {
-		constructor(name, surname) {
-			// if (!name || !surname) {
-			// 	throw new Error('Fields name and surname are required');
-			// };
-			this.name = name;
-			this.surname = surname;
-		};
-		getData() {
-			return `${this.name} ${this.surname}`;
-		};
-	};
+  class Person {
+    constructor(name, surname) {
+      // if (!name || !surname) {
+      // 	throw new Error('Fields name and surname are required');
+      // };
+      this.name = name;
+      this.surname = surname;
+    }
+    getData() {
+      return `${this.name} ${this.surname}`;
+    }
+  }
 
-	class Seat {
-		constructor(number = Math.floor( 91 * Math.random() + 10 ), category = "e") {
-			// if ( !['e','b'].includes(category) ) {
-			// 	throw new Error('Invalid category input');
-			// }
-			this.number = number;
-			this.category = category;
-		};
-		getData() {
-			return `${this.number} ${this.category.toUpperCase()}`;
-		};
-	};
+  class Seat {
+    constructor(number = Math.floor(91 * Math.random() + 10), category = "e") {
+      // if ( !['e','b'].includes(category) ) {
+      // 	throw new Error('Invalid category input');
+      // }
+      this.number = number;
+      this.category = category;
+    }
+    getData() {
+      return `${this.number} ${this.category.toUpperCase()}`;
+    }
+  }
 
-	class Passenger {
-		constructor(person, seat) {
-			// if (!person ||  !(person instanceof Person)) {
-			// 	throw new Error('Invalid person input');
-			// }
-			// if (!seat ||  !(seat instanceof Seat)) {
-			// 	throw new Error('Invalid seat input');
-			// }
-			this.person = person;
-			this.seat = seat;
-		};
-		getData() {
-			return `${this.seat.getData()} ${this.person.getData()}`;
-		};
-	};
-	
-	class Flight {
-		constructor(relation, date) {
-			// if (!relation || !date) {
-			// 	throw new Error('Fields relation and date are required');
-			// }
-			this.relation = relation;
-			this.date = new Date(date);
-			this.listOfPassengers = [];
-		};
-		addPassenger(passenger) {
-			// if (!passenger || !(passenger instanceof Passenger)) {
-			// 	throw new Error('Invalid passenger data');
-			// }
-			this.listOfPassengers.push(passenger);
-		};
-		getData() {
-			let year = this.date.getFullYear();
-			let month = this.date.getMonth() + 1;
-			let day = this.date.getDate();
-			let result = `  ${day}.${month}.${year}, ${this.relation}
+  class Passenger {
+    constructor(person, seat) {
+      // if (!person ||  !(person instanceof Person)) {
+      // 	throw new Error('Invalid person input');
+      // }
+      // if (!seat ||  !(seat instanceof Seat)) {
+      // 	throw new Error('Invalid seat input');
+      // }
+      this.person = person;
+      this.seat = seat;
+    }
+    getData() {
+      return `${this.seat.getData()} ${this.person.getData()}`;
+    }
+  }
+
+  class Flight {
+    constructor(relation, date) {
+      // if (!relation || !date) {
+      // 	throw new Error('Fields relation and date are required');
+      // }
+      this.relation = relation;
+      this.date = new Date(date);
+      this.listOfPassengers = [];
+    }
+    addPassenger(passenger) {
+      // if (!passenger || !(passenger instanceof Passenger)) {
+      // 	throw new Error('Invalid passenger data');
+      // }
+      this.listOfPassengers.push(passenger);
+    }
+    getData() {
+      let year = this.date.getFullYear();
+      let month = this.date.getMonth() + 1;
+      let day = this.date.getDate();
+      let result = `  ${day}.${month}.${year}, ${this.relation}
 			`;
-			this.listOfPassengers.forEach(function(passenger) {
-				result += `     ${passenger.getData()}
+      this.listOfPassengers.forEach((passenger) => {
+        result += `     ${passenger.getData()}
 				`;
-			});
-			return result;
-		};
-	};
-	
-	class Airport {
-		constructor() {
-			this.name = "Nikola Tesla";
-			this.listOfFlights = [];
-		};
-		addFlight(flight) {
-			// if (!flight || !(flight instanceof Flight)) {
-			// 	throw new Error('Invalid flight data');
-			// }
-			this.listOfFlights.push(flight);
-		};
-		getPassengersNumber() {
-			let count = 0;
-			this.listOfFlights.forEach(function(flight) {
-				count += flight.listOfPassengers.length;
-			});
-			return count;
-		}
-		getData() {
-			let result = `Airport: ${this.name}, total passengers: ${this.getPassengersNumber()}
+      });
+      return result;
+    }
+  }
+
+  class Airport {
+    constructor() {
+      this.name = "Nikola Tesla";
+      this.listOfFlights = [];
+    }
+    addFlight(flight) {
+      // if (!flight || !(flight instanceof Flight)) {
+      // 	throw new Error('Invalid flight data');
+      // }
+      this.listOfFlights.push(flight);
+    }
+    getPassengersNumber() {
+      let count = 0;
+      this.listOfFlights.forEach((flight) => {
+		  count += flight.listOfPassengers.length;
+		  return count;
+	  });
+    }
+    getData() {
+      let result = `Airport: ${
+        this.name
+      }, total passengers: ${this.getPassengersNumber()}
             `;
-			this.listOfFlights.forEach(function(flight) {
-				result += flight.getData();
-			});
-			return result;
-		};
-	};
-	
+      this.listOfFlights.forEach((flight) => {
+		  result += flight.getData();
+		  return count;
+	  });
+    }
+  }
 
+  const createFlight = (relation, date) => new Flight(relation, date);
 
-	function createFlight(relation, date) {
-		return new Flight(relation, date);
-	}
-	function createPassenger(firstName, lastName, seatNumber, category) {
-		let person = new Person(firstName, lastName);
-		let seat = new Seat(seatNumber, category);
-		return new Passenger(person, seat);
-	}
-	
-	//testing
-	try {
-		let nikolaTesla = new Airport()
-		let belgradeParis = createFlight("Belgrade - Paris", "Oct 25 2017")
-		let barcelonaBelgrade = createFlight("Barcelona - Belgrade", "Nov 11 2017")
-		let passenger1 = createPassenger("Dario", "Stamenkovic", 67, "b")
-		let passenger2 = createPassenger("Cersei", "Lannister", 62, "b")
-		let passenger3 = createPassenger("Daenerys", "Targaryen", 14)
-		let passenger4 = createPassenger("Nikola", "Colovic")
-		belgradeParis.addPassenger(passenger1)
-		belgradeParis.addPassenger(passenger2)
-		barcelonaBelgrade.addPassenger(passenger3)
-		barcelonaBelgrade.addPassenger(passenger4)
-		nikolaTesla.addFlight(belgradeParis)
-		nikolaTesla.addFlight(barcelonaBelgrade)
-		console.log(nikolaTesla.getData());
-	} catch (error) {
-		console.log(`Error message: ${error.message}`);
-	}
-})()
+  const createPassenger = (firstName, lastName, seatNumber, category) => {
+    let person = new Person(firstName, lastName);
+    let seat = new Seat(seatNumber, category);
+    return new Passenger(person, seat);
+  };
+
+  //testing
+  try {
+    let nikolaTesla = new Airport();
+    let belgradeParis = createFlight("Belgrade - Paris", "Oct 25 2017");
+    let barcelonaBelgrade = createFlight("Barcelona - Belgrade", "Nov 11 2017");
+    let passenger1 = createPassenger("Dario", "Stamenkovic", 67, "b");
+    let passenger2 = createPassenger("Cersei", "Lannister", 62, "b");
+    let passenger3 = createPassenger("Daenerys", "Targaryen", 14);
+    let passenger4 = createPassenger("Nikola", "Colovic");
+    belgradeParis.addPassenger(passenger1);
+    belgradeParis.addPassenger(passenger2);
+    barcelonaBelgrade.addPassenger(passenger3);
+    barcelonaBelgrade.addPassenger(passenger4);
+    nikolaTesla.addFlight(belgradeParis);
+    nikolaTesla.addFlight(barcelonaBelgrade);
+    console.log(nikolaTesla.getData());
+  } catch (error) {
+    console.log(`Error message: ${error.message}`);
+  }
+})();
---
+++ ES6/exercises.js
@@ -0,0 +1,104 @@
+// Write a function that capitalizes the first letter of each string argument it receives.  
+// 	Function arguments: ['hello', 'there', 'ES', 6]
+// 	Output: ['Hello', 'There', 'ES']
+
+const capitalizeFirstLetters = arguments => {
+    let result = [];
+    arguments.forEach((argument) => {
+        if(typeof argument == "string") {
+            result[result.length] = `${argument[0].toUpperCase()}${argument.substring(1)}`;
+        } 
+    });
+    return result;
+};
+let arr = ['hello', 'there', 467, "kukuleleleee", NaN, 'ES', 6];
+console.log(capitalizeFirstLetters(arr));
+
+
+// Write a function that calculates sale tax that should be paid for the product of the given price. 
+//Use a constant to set a fixed value of the tax rate (i.e. 20% in Serbia). 
+// 	Input: [{ name: “Banana”, price: 120 }, {name: “Orange”,  price: 100}]
+// 	Output: [{ name: “Banana”, price: 144 }, { name: “Orange”,  price: 120 }] // product full price
+// 	Output2: [ 24, 20 ] // tax only
+
+const products = [{ name: "Banana", price: 120.23, date: new Date() }, { name: "Orange", price: 100, date: new Date() }]
+ 
+const productsWithTax = products.map(product => {
+    const { price: prodPrice } = product;
+    const newProd = {
+        ...product,
+        price: parseFloat(prodPrice.toFixed(2)),
+        priceWithTax: parseFloat((prodPrice* 1.2).toFixed(2)),
+        tax: Math.floor(prodPrice * 0.2)
+    }
+    return newProd;
+})
+
+const taxesOnly = productsWithTax.map(product => {
+    const { price: prodPrice } = product;
+    return Math.floor(prodPrice * 0.2);
+})
+ 
+console.log(products);
+console.log(productsWithTax);
+console.log(taxesOnly);
+
+
+	
+// Write a function that increases each element of the given array 
+//by the given value. If the value is omitted, increase each element of the array by 1.  
+// 	Input: [4, 6, 11, -9, 2.1], 2
+// 	Output: [6, 8, 13, -7, 4.1]
+
+const increaseElement = (arr, value=1) => {
+    let result = [];
+    arr.forEach((element) => result[result.length] = element + value);
+    return result;
+};
+
+console.log(increaseElement([4, 6, 11, -9, 2.1], 2));
+console.log(increaseElement([4, 6, 11, -9, 2.1]));
+
+// Write a function that filters all even elements of the array.
+// 	Input: [6, 11, 9, 0, 3]
+// 	Output: [6, 0]
+
+// Write a function that filters all the strings from the given array that contain substring JS or js.
+// 	Input: ['compiler', 'transpiler', 'babel.js', 'JS standard', 'linter']
+// 	Output: ['babel.js, 'JS standard']
+
+// Write a function that filters all integer numbers from the given array. 
+// Input: [1.6, 11.34, 9.23, 7, 3.11, 8]
+// 	Output: [7, 8]
+
+// Write a function that filters all integer arguments that contain digit 5.
+// 	Function arguments: 23, 11.5, 9, 'abc', 45, 28, 553 
+// 	Output: [45, 553]
+	
+// Write a function that returns indexes of the elements greater than 10. 
+// 	Input: [1.6, 11.34, 29.23, 7, 3.11, 18]
+// 	Output: 1, 2, 5
+
+
+
+// Create an array of persons. A person should be an object with name and age properties. Experiment with enhanced object literals. 
+// Write a function that prints out the names of persons older than 25. 
+// Write a function that check if there is a person older than 40.
+// Write a function that checks if all persons are older than 20.   
+ 
+
+
+// Write a function that checks if the given array is an array of positive integer values. 
+// 	Input: [3, 11, 9, 5, 6]
+// 	Output: yes
+
+// 	Input: [3, -12, 4, 11]
+// 	Output: no
+
+// Write a function that calculates the product of the elements of the array. 
+// Input: [2, 8, 3]
+// Output:  48
+
+// Write a function that calculates the maximum of the given array. 
+// Input: [2, 7, 3, 8, 5.4] 
+// 	Output: 8
