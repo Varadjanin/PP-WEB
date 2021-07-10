cea4be4a697c03e81ec910bdd062368c624d052d
Varadjanin <mbodrozic@gmail.com>
2021-07-10 22:40:36 +0200

    adding movie festival exercise in ES6

--- festivalMovies/entities_movieFestival.js
+++ festivalMovies/entities_movieFestival.js
@@ -1,83 +1,77 @@
-
-function Movie(title, length, genre) {
-    if (!title) {
-        throw new Error('Field Title is required');
-    }
-    if (!length) {
-        throw new Error('Field Length is required');
-    }
-    if (typeof length === "string") {
-        throw new Error('The length of the movie should be a number.');
-    }
-    if (!genre) {
-        throw new Error('Field Genre is required');
-    }
-    this.title = title;
-    this.genre = genre;
-    this.length = length;
-    this.genreShort = genre[0].toUpperCase() + genre[genre.length-1].toUpperCase();             
-}
-
-Movie.prototype.getData = function () {
-    return this.title + ", " + this.length + "min, " + this.genreShort;
-}
-                
-                
-function Program(date) {
-    if (!date) {
-        throw new Error('Field Date is required');
-    }
-
-    var today = new Date(date);
-    var dd = today.getDate();
-    var mm = today.getMonth()+1; 
-    var yyyy = today.getFullYear();
-    if(dd<10){
-        dd='0'+dd
-    } 
-    if(mm<10){
-        mm='0'+mm
-    } 
-    today = dd+'-'+mm+'-'+yyyy;
-
-    this.date = today;
-    this.listOfMovies = [];
-}
-
-
-Program.prototype.numberOfMoviesInProgram =  function() {        
-    var count = 0;
-    this.listOfMovies.forEach(function() {
-        count ++;
-    })
-    return count;                   
-}
-
-Program.prototype.addMovie = function (movie) {
-    if (!movie || !(movie instanceof Movie)) {
-        throw new Error('Invalid movie data');
+class Movie{
+    constructor(title, length, genre) {
+        if (!title) {
+            throw new Error('Field Title is required');
+        }
+        if (!length) {
+            throw new Error('Field Length is required');
+        }
+        if (typeof length === "string") {
+            throw new Error('The length of the movie should be a number.');
+        }
+        if (!genre) {
+            throw new Error('Field Genre is required');
+        }
+        this.title = title;
+        this.genre = genre;
+        this.length = length;
+        this.genreShort = `${genre[0].toUpperCase() + genre[genre.length-1].toUpperCase()}`; 
     };
-    this.listOfMovies.push(movie);
-    return "Movie added.";
-}
+    getData() {
+        return `${this.title}, ${this.length}min, ${this.genreShort}`;
+    };
+};
 
-Program.prototype.getTotalLengthOfAllMoviesInTheProgram = function () {
-    var sum = 0;
-    this.listOfMovies.forEach(function(movie) {
-        sum += movie.length;
-    });
-    return sum + "min";
-}
 
-Program.prototype.getData = function () {
-    var result = this.date + ", " + this.listOfMovies.length + " movies, duration:" + this.getTotalLengthOfAllMoviesInTheProgram();
-    return result;
-}
+class Program{
+    constructor(date) {
+        if (!date) {
+            throw new Error('Field Date is required');
+        }
+        var today = new Date(date);
+        var dd = today.getDate();
+        var mm = today.getMonth()+1; 
+        var yyyy = today.getFullYear();
+        if(dd<10){
+            dd='0'+dd;
+        } 
+        if(mm<10){
+            mm='0'+mm;
+        } 
+        today = dd+'-'+mm+'-'+yyyy;
+        this.date = today;
+        this.listOfMovies = [];
+    };
+    numberOfMoviesInProgram() {
+        var count = 0;
+        this.listOfMovies.forEach(() => count++);
+        return count;
+    };
+    addMovie(movie) {
+        if (!movie || !(movie instanceof Movie)) {
+            throw new Error('Invalid movie data');
+        };
+        this.listOfMovies.push(movie);
+        return "Movie added.";
+    };
+    getTotalLengthOfAllMoviesInTheProgram() {
+        var sum = 0;
+        this.listOfMovies.forEach((movie) => sum += movie.length);
+        return `${sum}min`;
+    };
+    getData() {
+        var result = `${this.date}, ${this.listOfMovies.length} movies, duration: ${this.getTotalLengthOfAllMoviesInTheProgram()}`;
+        return result;
+    };
+};
 
-function Festival() {
-    this.allMovies = [];
-    this.allPrograms = [];
-}
+
+class Festival{
+    constructor() {
+        this.allMovies = [];
+        this.allPrograms = [];
+    };
+};
 
 
 //testing:
--- festivalMovies/forms_movieFestival.js
+++ festivalMovies/forms_movieFestival.js
@@ -1,34 +1,34 @@
 //creating festival:
-var festival = new Festival();
+const festival = new Festival();
 
 //selecting elements:
 
-var movieTitle = document.querySelector(".movie_title");
-var movieLength = document.querySelector(".movie_length");
-var movieGenre = document.querySelector(".movie_genre");
-var movieList = document.querySelector(".movie_list");
-var dropdownMovies = document.querySelector(".dropdown_movies");
+let movieTitle = document.querySelector(".movie_title");
+let movieLength = document.querySelector(".movie_length");
+let movieGenre = document.querySelector(".movie_genre");
+let movieList = document.querySelector(".movie_list");
+let dropdownMovies = document.querySelector(".dropdown_movies");
 
 
-var programDate = document.querySelector(".program_date");
-var programList = document.querySelector(".program_list");
-var dropdownPrograms = document.querySelector(".dropdown_programs");
+let programDate = document.querySelector(".program_date");
+let programList = document.querySelector(".program_list");
+let dropdownPrograms = document.querySelector(".dropdown_programs");
 
 //_____________________________________________________
 //create Movie:
 
 function createMovie() {
     
-    var newMovie = new Movie(movieTitle.value, parseInt(movieLength.value), movieGenre.value);
-    var index = festival.allMovies.push(newMovie);
+    let newMovie = new Movie(movieTitle.value, parseInt(movieLength.value), movieGenre.value);
+    let index = festival.allMovies.push(newMovie);
     
-    var newLi = document.createElement("li");
+    let newLi = document.createElement("li");
     newLi.innerHTML = newMovie.getData();
     movieList.appendChild(newLi);
     
     
     //adding movie to the dropdown list:
-    var movieOption = document.createElement("option");
+    let movieOption = document.createElement("option");
 
     movieOption.innerHTML = newMovie.title;
     movieOption.setAttribute("value", index);
@@ -47,17 +47,17 @@ function createMovie() {
 
 function createProgram() {
     
-    var newProgram = new Program(programDate.value);
-    var index = festival.allPrograms.push(newProgram);
+    let newProgram = new Program(programDate.value);
+    let index = festival.allPrograms.push(newProgram);
 
     
-    var newLi = document.createElement("li");
+    let newLi = document.createElement("li");
     newLi.innerHTML = newProgram.getData();
     // newLi.classList.add('added');
     programList.appendChild(newLi);
     
     //adding program to the dropdown list:
-    var option = document.createElement('option');
+    let option = document.createElement('option');
     option.setAttribute('value', index);
     option.innerHTML = newProgram.getData();
     dropdownPrograms.appendChild(option);
@@ -73,17 +73,17 @@ function createProgram() {
 
 function addMovieToTheProgram() {
     
-    var programListElements = document.querySelectorAll(".program_list li");
-    var programSelectOptions = document.querySelectorAll(".dropdown_programs option");
+    let programListElements = document.querySelectorAll(".program_list li");
+    let programSelectOptions = document.querySelectorAll(".dropdown_programs option");
 
-    var movieValue = parseInt(dropdownMovies.value);
-    var programValue = parseInt(dropdownPrograms.value);
+    let movieValue = parseInt(dropdownMovies.value);
+    let programValue = parseInt(dropdownPrograms.value);
 
     // console.log(movieValue); //broji sve napravljene filmove
     // console.log(programValue);//broji sve napravljene programe
 
-    var movie = festival.allMovies[movieValue-1];
-    var program = festival.allPrograms[programValue-1];
+    let movie = festival.allMovies[movieValue-1];
+    let program = festival.allPrograms[programValue-1];
     program.listOfMovies.push(movie);
 
     // console.log(movie);
@@ -94,11 +94,10 @@ function addMovieToTheProgram() {
 
 };
 
-var createMovieBtn = document.querySelector(".movie_create_button");
-var createProgramBtn = document.querySelector(".program_create_button");
-var addMovieToProgramBtn = document.querySelector(".add_movie_to_program_button");
+let createMovieBtn = document.querySelector(".movie_create_button");
+let createProgramBtn = document.querySelector(".program_create_button");
+let addMovieToProgramBtn = document.querySelector(".add_movie_to_program_button");
 
 createMovieBtn.addEventListener("click", createMovie);
 createProgramBtn.addEventListener("click", createProgram);
 addMovieToProgramBtn.addEventListener("click", addMovieToTheProgram); 
-
